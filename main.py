54af95f30398e54a7437c811246287d52c1bbeb3
chlhugo01 <59417093+chlhugo01@users.noreply.github.com>
2023-08-11 11:43:00 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def day_1():
+    print('hi')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    day_1()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
